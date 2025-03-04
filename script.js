cd3154907e6163d17fff8e607c1b19fb7201cc41
Mayur <MayurSingh190405@gmail.com>
2025-03-04 16:05:52 +0530

    moving rectangle box

---
+++ script.js
@@ -0,0 +1,12 @@
+// console.log("hey"); 
+
+window.addEventListener("mousemove", function (details) {
+    var rect = this.document.querySelector(".rect")
+
+    var val = gsap.utils.mapRange(0, window.innerWidth, 50 + rect.getBoundingClientRect().width / 2, window.innerWidth - (50 + rect.getBoundingClientRect().width / 2), details.clientX)
+    
+    gsap.to(".rect", {
+        left: val + "px",
+        ease: Power3,
+    })
+})
